60d0c861ee3f028a19ff0408aedd7690a36c6ebc
RubberizedDuck <dyl_2536@hotmail.com>
2022-10-09 19:21:56 -0700

    Add unique instance for FileStorage

---
+++ models/__init__.py
@@ -0,0 +1,9 @@
+#!/bin/usr/python3
+"""
+This module creates a unique FileStorage instance
+for our application
+"""
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
