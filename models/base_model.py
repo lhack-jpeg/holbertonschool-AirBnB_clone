2125d1244c8706d7fadc316c53c9f48e66cf5cdb
RubberizedDuck <dyl_2536@hotmail.com>
2022-10-09 19:22:14 -0700

    Update: save & __init__

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ This module contains the super class BaseModel
 """
 import uuid
 from datetime import datetime
+from models import storage
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
 
@@ -27,6 +28,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            self.storage.new()
 
     def __str__(self):
         """
@@ -39,6 +41,7 @@ class BaseModel:
         Updates the public attribute 'updated_at' with current time
         """
         self.updated_at = datetime.now()
+        self.storage.save()
 
     def to_dict(self):
         """
