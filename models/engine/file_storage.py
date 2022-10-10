1c7ddd37acfb05fcb074b616492a2fa77c729e2e
RubberizedDuck <dyl_2536@hotmail.com>
2022-10-09 21:23:47 -0700

    Fix: add dict for classes in reload

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,30 +10,40 @@ class FileStorage:
     '''This is the constructor for the file storage class.'''
     def __init__(self):
         '''Initalise the Filestoage class'''
-        self.__file_path = dope_file_path_name.json
+        self.__file_path = "dope_file_path_name.json"
         self.__objects = {}
 
     def all(self):
         '''Returns the dictionary __objects'''
+        self.reload()
         return self.__objects
 
     def new(self, obj):
         '''Add new item to obj dictionary.'''
         if obj is not None:
-            obj_class = obj.__class__.__name__
-            self.__objects[obj_class] = obj.id
+            obj_class = obj.__class__.__name__ + "." + obj.id
+            self.__objects[obj_class] = obj
 
     def save(self):
         '''Writes the __objects dictionary to file.'''
+        data = {}
+        for key, value in self.__objects.items():
+            data[key] = value.to_dict()
         with open(self.__file_path, 'w', encoding='utf-8') as json_file:
-            json.dumps(self.__filepath, self.all)
+            json.dump(data, json_file)
 
     def reload(self):
         '''Reads the file and deserialise the file. If file doesn't exist do
         nothing.'''
-
+        filename = self.__file_path
+        class_dict = {
+            "BaseModel": BaseModel
+            }
         try:
-            with open(self.__file_path, 'r', encoding='utf-8') as json_file:
-                json.loads(self.__filepath)
-        except Exception:
+            with open(filename, 'r') as json_file:
+                json_dict = json.load(json_file)
+                for key, value in json_dict.items():
+                    self.__objects[key] = class_dict[value["__class__"]](
+                        **value)
+        except FileNotFoundError:
             pass
