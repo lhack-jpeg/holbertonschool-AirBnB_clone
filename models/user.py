9971cea28a3faba7e0acc2a01b2e2ca84b54e5d5
RubberizedDuck <dyl_2536@hotmail.com>
2022-10-11 16:44:16 -0700

    Fix: Add args kwargs to init

--- models/user.py
+++ models/user.py
@@ -5,13 +5,17 @@ the basemodel class.
 '''
 
 from models.base_model import BaseModel
-from models import storage
+import models
 
 class User(BaseModel):
     '''Constructor for User class.'''
-    def __init__(self):
-        super().__init__():
+    def __init__(self, *args, **kwargs):
+        super().__init__()
         self.email = ""
         self.password = ""
         self.first_name = ""
         self.last_name = ""
+
+    def __str__(self):
+        """Changes the str return for class"""
+        return f"[User] ({self.id}) {self.__dict__}"
