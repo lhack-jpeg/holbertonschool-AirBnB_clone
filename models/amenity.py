17e2e4f87a8ac4faffe9672e51f6fb40ce76a9b5
RubberizedDuck <dyl_2536@hotmail.com>
2022-10-11 17:34:08 -0700

    Add amenity class

---
+++ models/amenity.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+This module contains the amenity constructor that inherits from
+the basemodel class.
+'''
+
+from models.base_model import BaseModel
+import models
+
+class User(BaseModel):
+    '''Constructor for Amenity class.'''
+    name = ""
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+
+    def __str__(self):
+        """Changes the str return for class"""
+        return f"[Amenity] ({self.id}) {self.__dict__}"
