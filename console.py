3301298e4708bb98056b38e9dcfe137d18823f15
Luke Hackett <luke-hackett@outlook.com>
2022-10-12 16:50:43 -0700

    add count functionality

--- console.py
+++ console.py
@@ -136,6 +136,20 @@ class HBNBCommand(cmd.Cmd):
         else:
             print("** class doesn't exist **")
 
+    def do_count(self, arg):
+        '''
+        Prints out the count of the classes.
+        Usage: <class_name>.count()
+        '''
+        objs_list = []
+        all_objs = models.storage.all()
+        args = arg.split()
+
+        for key in all_objs:
+            if all_objs[key].__class__.__name__ == args[0]:
+                objs_list.append(all_objs[key].__str__())
+        print(len(objs_list))
+
     def do_update(self, arg):
         """
         Updates an instance based on class name and class id
@@ -178,6 +192,9 @@ class HBNBCommand(cmd.Cmd):
                 if args[0] in self.class_list:
                     if args[1] == 'all()':
                         self.do_all(args[0])
+                        return
+                    if args[1] == 'count()':
+                        self.do_count(args[0])
                         return
 
 if __name__ == '__main__':
