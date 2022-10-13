fb8bf17cb324019915ad09914aa124366ee42ae0
Luke Hackett <luke-hackett@outlook.com>
2022-10-13 15:13:07 -0700

    add dummy

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -51,6 +51,7 @@ class TestFileStorage(unittest.TestCase):
         '''Test the __file_path exists.'''
         file_path = self.storage._FileStorage__file_path
         self.assertIsNotNone(file_path)
+        print(file_path)
         self.assertEqual(file_path, "file.json")
 
     def test_file_storage_objects(self):
